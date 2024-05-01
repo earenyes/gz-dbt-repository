700e633c9b3b7df7acdf098e7a9c88dfe5bab0df
Eudald Arenas <eudaldarenyes@gmail.com>
2024-05-01 14:57:41 +0000

    cc

--- models/mart/finance/finance_campaigns_day.sql
+++ models/mart/finance/finance_campaigns_day.sql
@@ -1,10 +1,11 @@
 
+
 SELECT
-    campaigns.*,
-    finance.*,
-    (finance.operational_margin - campaigns.ads_costs) AS ads_margin
-FROM 
-    {{ ref('int_campaigns_day') }} AS campaigns
-JOIN 
-    {{ ref('finance_days') }} AS finance
-USING (date_date) 
+    date_date,
+    operational_margin - ads_costs AS ads_margin,
+    average_basket,
+    operational_margin,
+FROM {{ref("int_campaigns_day")}}
+LEFT JOIN {{ref("finance_days")}}
+USING (date_date)
+ORDER BY date_date DESC
--- models/mart/finance/finance_campaigns_month.sql
+++ models/mart/finance/finance_campaigns_month.sql
@@ -1,8 +1,8 @@
 SELECT
-    DATE_TRUNC(date_date, MONTH) AS month,
-    SUM(ads_costs) AS total_ads_costs,
-    SUM(ads_margin) AS ads_margin,
-    SUM(operational_margin) AS operational_margin,
-    SUM(average_basket) AS average_basket
-FROM 
-    {{ ref('finance_campaigns_day') }}
+    DATE_TRUNC(date_date,MONTH) as datemonth,
+    sum(ads_margin) AS ads_margin,
+    sum(average_basket) AS average_basket,
+    sum(operational_margin) AS operational_margin,
+FROM {{ref("finance_campaigns_day")}}
+GROUP BY datemonth
+ORDER BY datemonth DESC
