e95119eee94f71e9bc5e64fe42b754793a135237
Eudald Arenas <eudaldarenyes@gmail.com>
2024-05-01 13:28:01 +0000

    casted ads costs

--- models/staging/raw/stg_raw__raw_gz_adwords.sql
+++ models/staging/raw/stg_raw__raw_gz_adwords.sql
@@ -9,6 +9,13 @@ source as (
 renamed as (
 
     select
+        date_date,
+        paid_source,
+        campaign_key,
+        campgn_name,
+        CAST(ads_cost as FLOAT64) as ads_cost,
+        impression,
+        click
 
     from source
 
